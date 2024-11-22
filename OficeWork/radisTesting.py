ba66fa47943b05a55caabeaf49a0acf064ab2734
Rajni <rajni1990bhatia@gmail.com>
2024-11-22 21:31:15 +0530

    new Designation added

--- OficeWork/radisTesting.py
+++ OficeWork/radisTesting.py
@@ -6,9 +6,10 @@ r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 # Store data
 r.set("name", "Alice")
 r.set("age", "30")
-
+r.set("Designation","Software Enginer")
 # Retrieve data
 name = r.get("name")
 age = r.get("age")
+Designation=r.get("Designation")
 
-print(f"Name: {name}, Age: {age}")
+print(f"Name: {name}, Age: {age}, Designation: {Designation}")
